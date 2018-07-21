ddad1ec8b85671111b25aa56b764ddaf92357865
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2018-07-21 09:02:35 -0300

    Page attributes - It should validate that url and content cannot be empty/falsy

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Page, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'attributes' do
+    %i[url content].each do |field|
+      it { is_expected.to validate_presence_of field }
+    end
+  end
 end
